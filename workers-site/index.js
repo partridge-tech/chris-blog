8f326d1d9aa1be01ab9a4216430879dd64c8612d
Chris Partridge <chris@partridge.tech>
2020-05-26 12:36:47 -0400

    Add security headers & modified DEBUG practices to serverless

--- workers-site/index.js
+++ workers-site/index.js
@@ -4,35 +4,38 @@ import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
  * This is a downstream, modified version of the following repo:
  * https://github.com/cloudflare/worker-sites-template
  * 
- * The only changes of note are:
- * 1. this adds passThroughOnException() to all events, so if the Workers KV
+ * Some changes of note are:
+ * 1. This adds passThroughOnException() to all events, so if the Workers KV
  *    is down, data will be served from a backup source (GitLab Pages).
  * 2. 404s are served from 404/, not 404.html, which is a bad practice but
  *    is broken on GitLab Pages anyway, per
  *    https://gitlab.com/gitlab-org/gitlab-pages/-/issues/183
  *    ...and I might fix this to be 404.html when it's resolved.
- * 
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404/ page.
+ * 3. DEBUG flag has been removed in favor of always-on verbose errors since:
+ *      a. There is no sensitive content on this site. Not even an API.
+ *      b. Cache purges are easier to execute in the Cloudflare Dashboard.
+ * 4. The addition of createResponseWithHeaders() which adds security headers
+ *    to 200 & 404 requests, setting some useless-to-us headers such as:
+ *      a. XSS Protection, despite a total lack of JS on this site.
+ *      b. Content Type Options, despite no dynamic/user-uploaded content.
+ *      c. Frame Options, so you need to clone this repo instead of framing it.
+ *    ... and a couple actually useful ones:
+ *      d. Referrer Policy so only secure sites can know where you came from.
+ *      e. Content Security Policy, to prevent external resources loading.
+ *    Why? Because anything less than an A grade on https://securityheaders.io
+ *    is sacrilege. ;)
  */
-const DEBUG = false
 
 addEventListener('fetch', event => {
   event.passThroughOnException();
   try {
     event.respondWith(handleEvent(event))
   } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
-    }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
+    return event.respondWith(
+      new Response(e.message || e.toString(), {
+        status: 500,
+      }),
+    )
   }
 })
 
@@ -41,25 +44,30 @@ async function handleEvent(event) {
   let options = {}
 
   try {
-    if (DEBUG) {
-      // customize caching
-      options.cacheControl = {
-        bypassCache: true,
-      }
-    }
-    return await getAssetFromKV(event, options)
+    const page = await getAssetFromKV(event, options)
+    return createResponseWithHeaders(page.body, page)
   } catch (e) {
     // if an error is thrown try to serve the asset at 404/
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404/index.html`, req),
-        })
+    try {
+      let notFoundResponse = await getAssetFromKV(event, {
+        mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404/index.html`, req),
+      })
 
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
-    }
+      return createResponseWithHeaders(notFoundResponse.body, { ...notFoundResponse, status: 404 })
+    } catch (e) {}
 
     return new Response(e.message || e.toString(), { status: 500 })
   }
+}
+
+function createResponseWithHeaders(pageBody, page) {
+  const response = new Response(pageBody, page)
+
+  response.headers.set('X-XSS-Protection', '1; mode=block')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('Content-Security-Policy', 'default-src https://chris.partridge.tech:443')
+  response.headers.set('Referrer-Policy', 'no-referrer-when-downgrade')
+
+  return response
 }
