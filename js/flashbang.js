67384d92a79315fda295342839f8eeafaad7193e
Chris Partridge <chris@partridge.tech>
2020-11-17 22:26:56 -0500

    Fix CSP for real

--- js/flashbang.js
+++ js/flashbang.js
@@ -1,24 +1,27 @@
 /**
  * js/flashbang.js
- * Optional script which allows a user to toggle to light mode for readability.
+ * Optional script which allows a user to toggle to light mode for "readability."
+ * Personally, I find it blinding. Hence the name.
  * Blocking this script, or JavaScript entirely, will not impact site usability.
  * Thanks to Derek Kedziora. https://derekkedziora.com/blog/dark-mode-revisited
  */
+
+document.getElementById("flashbang-button").addEventListener("click", modeSwitcher);
 
 function modeSwitcher() {
     theme = sessionStorage.getItem('theme');
     if (theme === "dark") {
         document.documentElement.setAttribute('data-theme', 'light');
         sessionStorage.setItem('theme', 'light');
-        document.getElementById("flashbang").className = "entities-sym-moon";
+        document.getElementById("flashbang-icon").className = "entities-sym-moon";
     } else if (theme === "light") {
         document.documentElement.setAttribute('data-theme', 'dark');
         sessionStorage.setItem('theme', 'dark');
-        document.getElementById("flashbang").className = "entities-sym-sun";
+        document.getElementById("flashbang-icon").className = "entities-sym-sun";
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         sessionStorage.setItem('theme', 'light');
-        document.getElementById("flashbang").className = "entities-sym-moon";
+        document.getElementById("flashbang-icon").className = "entities-sym-moon";
     }
 }
 
@@ -27,13 +30,13 @@ let theme = sessionStorage.getItem('theme');
 if (theme === "dark") {
     document.documentElement.setAttribute('data-theme', 'dark');
     sessionStorage.setItem('theme', 'dark');
-    document.getElementById("flashbang").className = "entities-sym-sun";
+    document.getElementById("flashbang-icon").className = "entities-sym-sun";
 } else if (theme === "light") {
     document.documentElement.setAttribute('data-theme', 'light');
     sessionStorage.setItem('theme', 'light');
-    document.getElementById("flashbang").className = "entities-sym-moon";
+    document.getElementById("flashbang-icon").className = "entities-sym-moon";
 } else {
     document.documentElement.setAttribute('data-theme', 'dark');
     sessionStorage.setItem('theme', 'dark');
-    document.getElementById("flashbang").className = "entities-sym-sun";
+    document.getElementById("flashbang-icon").className = "entities-sym-sun";
 }
