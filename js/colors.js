3478b5c29edc2c555c617d7965d4041aa0827f93
Chris Partridge <chris@partridge.tech>
2020-11-17 21:48:58 -0500

    Update icons, placement, ready to deploy

--- js/colors.js
+++ js/colors.js
@@ -4,28 +4,28 @@ function modeSwitcher() {
     if (theme === "dark") {
         document.documentElement.setAttribute('data-theme', 'light');
         sessionStorage.setItem('theme', 'light');
-        document.getElementById("theme-toggle").innerText = "darken";
+        document.getElementById("theme-toggle").className = "entities-sym-moon";
     } else if (theme === "light") {
         document.documentElement.setAttribute('data-theme', 'dark');
         sessionStorage.setItem('theme', 'dark');
-        document.getElementById("theme-toggle").innerText = "brighten";
+        document.getElementById("theme-toggle").className = "entities-sym-sun";
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
         sessionStorage.setItem('theme', 'light');
-        document.getElementById("theme-toggle").innerText = "darken";
+        document.getElementById("theme-toggle").className = "entities-sym-moon";
     }
 }
 
 if (theme === "dark") {
     document.documentElement.setAttribute('data-theme', 'dark');
     sessionStorage.setItem('theme', 'dark');
-    document.getElementById("theme-toggle").innerText = "brighten";
+    document.getElementById("theme-toggle").className = "entities-sym-sun";
 } else if (theme === "light") {
     document.documentElement.setAttribute('data-theme', 'light');
     sessionStorage.setItem('theme', 'light');
-    document.getElementById("theme-toggle").innerText = "darken";
+    document.getElementById("theme-toggle").className = "entities-sym-moon";
 } else {
     document.documentElement.setAttribute('data-theme', 'dark');
     sessionStorage.setItem('theme', 'dark');
-    document.getElementById("theme-toggle").innerText = "brighten";
+    document.getElementById("theme-toggle").className = "entities-sym-sun";
 }
